5b13d3a67af2247657acb2c0acc7c7c75ac8fb1f
System Administrator <admin@example.com>
2025-07-10 14:30:54 -0400

    scripted Rest api is added

---
+++ src/fluent/generated/sys_ws_definition_5a7eaa534726a2508295f19f016d43a0.now.ts
@@ -0,0 +1,25 @@
+import { RestApi } from '@servicenow/sdk/core'
+
+export default RestApi({
+    $id: 'generated0',
+    enforce_acl: ['cf9d01d3e73003009d6247e603f6a990'],
+    name: 'Test',
+    service_id: 'test',
+    routes: [
+        {
+            name: 'gitdemo',
+            consumes: 'application/json,application/xml,text/xml',
+            produces: 'application/json,application/xml,text/xml',
+            path: '/demo',
+            enforce_acl: ['cf9d01d3e73003009d6247e603f6a990'],
+            script: script`
+                 (function process(/*RESTAPIRequest*/ request, /*RESTAPIResponse*/ response) {
+                 
+                     // implement resource here
+                 	response.setStatus(200);
+                 
+                 })(request, response);`,
+            $id: 'generated1',
+        },
+    ],
+})
